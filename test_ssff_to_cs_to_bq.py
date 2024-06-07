4aaf74a8f4d0f31645f1e9d648f7287f48093678
Alan Zapata <adzapata@corpcab.com.mx>
2024-06-07 17:25:45 -0600

    Se eliminan tests, se crea test de ssff a cs a bq

--- test_ssff_to_cs_to_bq.py
+++ test_ssff_to_cs_to_bq.py
@@ -9,6 +9,8 @@ from datetime import datetime, timedelta
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from google.cloud import storage
+from google.cloud import bigquery
 import pytz
 import xml.etree.ElementTree as ET
 import pyarrow as pa
@@ -22,6 +24,13 @@ PAGE_SIZE = 1000  # Tamaño de página
 MAX_NUMBER_OF_PAGES = 1000
 DATE_FILTER_CONDITIONS = "fromDate=1900-01-01"
 
+#CONSTANTES DE CLOUD STORAGE
+BUCKET_NAME = 'raw_ssff_mx_qa'
+
+#CONSTANTES DE BIGQUERY
+PROJECT_ID = "psa-sga-dfn-qa" # CAMBIAR A PRD TRAS PRUEBAS: "psa-sga-dfn-pr"
+DATASET_ID = "raw_ssff_mx"
+
 def get_api_data(odata_base_url, page_size, username, password, date_filter_conditions, max_number_of_pages, endpoint):
     #Lista vacía para llenarla con los registros encontrados
     all_data = []
@@ -34,8 +43,8 @@ def get_api_data(odata_base_url, page_size, username, password, date_filter_cond
         return new_column_name
     
     while current_page_number <= max_number_of_pages:
+
         url = f"{odata_base_url}{endpoint}?$top={page_size}&$skip={(current_page_number - 1) * page_size}&{date_filter_conditions}"
-        
         response = requests.get(url, auth=(username, password))
         data = response.text
         # Creo siempre en estos casos va a estar en XML (asegurar)
@@ -50,10 +59,44 @@ def get_api_data(odata_base_url, page_size, username, password, date_filter_cond
                 entry_dict[elem.tag.replace('{http://schemas.microsoft.com/ado/2007/08/dataservices/metadata}', '')] = elem.text
             entry_data.append(entry_dict)
         
-        all_data = all_data.extend(entry_data)
+        all_data.extend(entry_data)
         
         current_page_number += 1
-    
-    return all_data
 
-dic_result = get_api_data(ODATA_BASE_URL, PAGE_SIZE, SSFF_USERNAME, SSFF_PASSWORD, DATE_FILTER_CONDITIONS, MAX_NUMBER_OF_PAGES, 'Position')
+    table = pa.Table.from_pylist(all_data)
+    new_column_names = [transform_column_name(name) for name in table.schema.names]
+    table = table.rename_columns(new_column_names)
+    
+    return table
+
+def table_to_CS(table, table_id):
+    temp_path = f"/tmp/{table_id}.parquet"
+    path_blob = f"{table_id}.parquet"
+    pq.write_table(table, temp_path)
+    # Crear cliente storage
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(path_blob)
+    # Cargar parquet a CS
+    blob.upload_from_filename(temp_path)
+
+def CS_to_BQ(table_id_name):
+    client = bigquery.Client()
+    table_id = f"{PROJECT_ID}.{DATASET_ID}.{table_id_name}"
+    gcs_uri = f"gs://{BUCKET_NAME}/{table_id_name}.parquet"
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.PARQUET,
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+    )
+    # Cargar de GC a BQ
+    load_job = client.load_table_from_uri(gcs_uri, table_id, job_config=job_config)
+    # Esperar al que trabajo se haga
+    load_job.result()
+
+
+dic_result = get_api_data(ODATA_BASE_URL, PAGE_SIZE, SSFF_USERNAME, SSFF_PASSWORD, DATE_FILTER_CONDITIONS, MAX_NUMBER_OF_PAGES, 'PayScaleGroup')
+table_to_CS(dic_result, 'payscalegroup')
+CS_to_BQ('payscalegroup')
+
+
+
